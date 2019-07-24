05db74410d6ffd6bcc29dc18cb13bd7a4c165429
taufiq <taufiq1689@gmail.com>
2019-07-24 14:30:46 +0700

    add product to product list

--- src/screen/stock/AddProductScreen.js
+++ src/screen/stock/AddProductScreen.js
@@ -1,32 +1,55 @@
 import React from 'react'
-import {View,Text,StyleSheet,Dimensions,TextInput,Button} from 'react-native'
+import {View,Text,StyleSheet,Dimensions,TextInput,Button,AsyncStorage} from 'react-native'
 import { TextInputMask } from 'react-native-masked-text'
 export default class AddProductScreen extends React.Component {
     constructor() {
         super()
     
         this.state = {
-          simple: ''
+            productName: '',    
+            buyPrice: '',
+            sellPrice: ''
         }
-      }
+    }
     static navigationOptions = {
         title:'Tambah Produk',
     }
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Tambah Produk</Text>
+            <Text style= {{marginBottom:15}}>Tambah Produk</Text>
             <TextInput
                 style={styles.TextInput}
                 placeholder="Nama Produk"
+                value = {this.state.productName}
+                onChangeText={text=>{
+                    this.setState({
+                        productName: text
+                    })
+                }}
             />
-
             <TextInputMask
                 type={'money'}
-                value={this.state.simple}
+                value={this.state.buyPrice}
                 onChangeText={text => {
                     this.setState({
-                    simple: text
+                    buyPrice: text
+                    })
+                }}
+                options={{
+                    unit: '',
+                    precision: 0,
+                    suffixUnit: ''
+                }}
+                style = {styles.TextInput}
+                placeholder= "Harga Beli"
+            />
+            <TextInputMask
+                type={'money'}
+                value={this.state.sellPrice}
+                onChangeText={text => {
+                    this.setState({
+                    sellPrice: text
                     })
                 }}
                 options={{
@@ -35,23 +58,60 @@ export default class AddProductScreen extends React.Component {
                     suffixUnit: ''
                 }}
                 style = {styles.TextInputLast}
-                placeholder= "Masukan Harga"
+                placeholder= "Harga Jual"
             />
 
 
-            <Button title="Tambahkan" onPress={coba} ></Button>
+            <Button title="Tambahkan" onPress={this.addProduct} ></Button>
+            {/* <Button title="Check" onPress={this.checkProduct} ></Button> */}
         </View>
       );
     }
+    // checkProduct = async () => {
+    //     AsyncStorage.getItem('UID123').then((value)=>{
+    //         console.warn(value);
+    //     })
+    //   };
+    addProduct = ()=>{
+        AsyncStorage.getItem('products').then((value)=>{
+            if (!value) {
+                products = []
+                product = {
+                    name: this.state.productName,
+                    sellPrice: this.state.sellPrice,
+                    buyPrice: this.state.buyPrice
+                }
+                products.push(product)
+
+                products = JSON.stringify(product)
+                AsyncStorage.setItem('products', products).then((value)=>{
+                    
+                })
+            }
+            else{
+                AsyncStorage.getItem('products').then((value)=>{
+                    console.warn(value);
+                    
+                    products = JSON.parse(value);
+                    product = {
+                        name: this.state.productName,
+                        sellPrice: this.state.sellPrice,
+                        buyPrice: this.state.buyPrice
+                    }
+
+                    products.push(product)
+                    products = JSON.stringify(products)
+                    
+                    AsyncStorage.setItem('products', products).then((value)=>{
+                        this.props.navigation.goBack()
+                    })
+                })
+            }
+        })
+
+    }
 }
 
-function berubah() {
-    return console.warn("berubah")
-}
-function coba() {
-    return console.warn("haha");
-    
-}
 const styles = StyleSheet.create({
     TextInput: {
       height: 50,
--- src/screen/stock/StockScreen.js
+++ src/screen/stock/StockScreen.js
@@ -1,24 +1,64 @@
 import React from 'react'
-import {View,Text,TouchableOpacity,StyleSheet} from 'react-native'
+import {View,Text,TouchableOpacity,Image,StyleSheet,FlatList,AsyncStorage} from 'react-native'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 export default class StockScreen extends React.Component {
+    constructor() {
+      super()
+
+      this.state = {
+        productFinish: false,
+        listProduct: [
+        ]
+      }
+    }
     static navigationOptions = {
         title:'Stock Screen',
         header: null
     }
+
+    componentDidMount(){
+      AsyncStorage.getItem('products').then((value)=>{
+        
+        console.warn(value);
+        
+        products = JSON.parse(value)
+        products.forEach(product => {
+          this.state.listProduct.push({name: product.name, sellPrice: product.sellPrice})
+        });
+      })
+    }
+    
     render() {
         const {navigate} = this.props.navigation;
+        
       return (
-        <View style={{ position:'relative',flex: 1,  alignItems: 'center',paddingTop:15 }}>
-          <Text style={{fontSize:24}}>List Produk</Text>
+        <View style={{position:'relative',flex: 1}}>
+          <View style={{ position:'relative',  alignItems: 'center',paddingTop:15,marginBottom:30 }}>
+            <Text style={{fontSize:24}}>List Produk</Text>
+          </View>
+          <FlatList
+          data= {this.state.listProduct}
+          extraData = {this.state.listProduct}
+          renderItem={({item}) =>
+              <View style={{ flexDirection: 'row', alignItems:'flex-start',padding:8,borderBottomWidth:0.3,borderBottomColor:'#d6d6d6'}}>
+                    <Image
+                      style={{width: 80, height: 80,marginRight:10,borderRadius:2}}
+                      source={{uri: 'https://miro.medium.com/max/1838/0*f81bU2qWpP51WWWC.jpg'}}
+                    />
+                    <View style={{flex:1}}>
+                      <Text style={{fontSize:22}}>{item.name}</Text>
+                      <Text style={{color:'gray'}}>{item.sellPrice}</Text>
+                    </View>
+              </View>}
+        />
           <TouchableOpacity
-            activeOpacity={0.7}
-            onPress={this.clickHandler}
-            style={styles.TouchableOpacityStyle}
-            onPress={() => this.props.navigation.navigate('addProduct')}>
-  
-            <FontAwesomeIcon icon={faPlus} color="white" />
+              activeOpacity={0.7}
+              onPress={this.clickHandler}
+              style={styles.TouchableOpacityStyle}
+              onPress={() => this.props.navigation.navigate('addProduct')}>
+    
+              <FontAwesomeIcon icon={faPlus} color="white" />
           </TouchableOpacity>
         </View>
       );
