142aec50e4bc18889e0e5d47d3b511e9795d65ab
taufiq <taufiq1689@gmail.com>
2019-07-24 15:45:36 +0700

    add store,provider,and reducer

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import redux from './src/reducers/Reducerx'
 
-AppRegistry.registerComponent(appName, () => App);
+const store = createStore(redux)
+const AppContainer = ()=>
+    <Provider store={store}>
+        <App/>
+    </Provider>
+
+AppRegistry.registerComponent(appName, () => AppContainer);
